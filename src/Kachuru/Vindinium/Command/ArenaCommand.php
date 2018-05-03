68e811af7aebce493373f9c3758c2f1094d527e1
Rupert Jones <git@kachuru.uk>
2018-05-03 13:27:59 +0100

    Use BotFactory in command classes

--- src/Kachuru/Vindinium/Command/ArenaCommand.php
+++ src/Kachuru/Vindinium/Command/ArenaCommand.php
@@ -4,6 +4,7 @@ namespace Kachuru\Vindinium\Command;
 
 use Kachuru\Base\Command\Command;
 use Kachuru\Vindinium\Bot\BasicBot;
+use Kachuru\Vindinium\Bot\BotFactory;
 use Kachuru\Vindinium\Bot\CleverBot;
 use Kachuru\Vindinium\Client;
 use Symfony\Component\Console\Input\InputInterface;
@@ -13,10 +14,15 @@ use Symfony\Component\Console\Output\OutputInterface;
 class ArenaCommand extends Command
 {
     private $client;
+    /**
+     * @var BotFactory
+     */
+    private $botFactory;
 
-    public function __construct(Client $client)
+    public function __construct(Client $client, BotFactory $botFactory)
     {
         $this->client = $client;
+        $this->botFactory = $botFactory;
         parent::__construct();
     }
 
@@ -30,9 +36,7 @@ class ArenaCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $bot = $input->getOption('bot') == 'basic'
-            ? new BasicBot()
-            : new CleverBot();
+        $bot = $this->botFactory->getBotByHandle($input->getOption('bot'));
 
         $this->client->startArena($bot, $input->getOption('games'));
     }
